3db7bf2a7b33877f1afc8fa9a65242d1d361966d
alphaliru <42791058+alphaliru@users.noreply.github.com>
2025-08-11 23:44:25 +0800

    fix:delete some file

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-50 text-neutral-800">
-      <h1 className="text-3xl font-bold">此時甜點官網 - GitHub Pages 測試</h1>
-    </div>
-  )
-}
